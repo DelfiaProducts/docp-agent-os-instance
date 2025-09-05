d79b2e9579f3a20a274802b281cd388a326e5a42
Tom <wellington.gadelha@delfia.tech>
2025-09-05 14:48:23 -0300

    #1[refac: libs/pkg/constants.go]: add constant url release

--- libs/pkg/constants.go
+++ libs/pkg/constants.go
@@ -1,8 +1,10 @@
 package pkg
 
 const (
-	DOCP_AGENT_PORT               = "12012"
-	DOCP_DOMAIN                   = "https://msapi.sandbox.docphq.tech"
-	DOCP_BINARIES_REPO            = "https://test-docp-agent-data.s3.amazonaws.com"
+	DOCP_AGENT_PORT = "12012"
+	DOCP_DOMAIN     = "https://msapi.sandbox.docphq.tech"
+	BUCKET_URL      = "https://docp-agent-k8s.s3.us-east-1.amazonaws.com"
+	URL_RELEASE     = "https://github.com/DelfiaProducts/docp-agent-os-instance/releases/download"
+
 	DOCP_FILE_AGENT_VERSIONS_NAME = "index.json"
 )
