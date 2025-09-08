436f74b32b7dd764cd4426ef272114428e44f961
Tom <wellington.gadelha@delfia.tech>
2025-09-08 15:33:03 -0300

    #1[refac: installers/manager/install_manager_windows.go]: add utility service for get version latest

--- installers/manager/install_manager_windows.go
+++ installers/manager/install_manager_windows.go
@@ -12,6 +12,8 @@ import (
 	"syscall"
 	"unsafe"
 
+	"github.com/DelfiaProducts/docp-agent-os-instance/libs/services"
+	"github.com/DelfiaProducts/docp-agent-os-instance/libs/utils"
 	"gopkg.in/yaml.v2"
 )
 
@@ -82,15 +84,6 @@ func processTags(tags string) map[string]string {
 		}
 	}
 	return tagMap
-}
-
-// formatTagsForYAML recebe um slice de tags e formata para a estrutura YAML desejada
-func formatTagsForYAML(tags []string) string {
-	var formattedTags []string
-	for _, tag := range tags {
-		formattedTags = append(formattedTags, fmt.Sprintf("%s", tag))
-	}
-	return strings.Join(formattedTags, "\n")
 }
 
 // addContentConfigYml save initial config yml
@@ -166,6 +159,19 @@ func main() {
 	parseParams(&apiKey, &tags, &version, &noGroupAssociation)
 	baseUrl := "https://github.com/DelfiaProducts/docp-agent-os-instance/releases/download"
 	fileName := "manager-windows-amd64.exe"
+	//verify if version latest
+	if version == "latest" {
+		logger := utils.NewDocpLoggerText(os.Stdout)
+		utilityService := services.NewUtilityService(logger)
+		if err := utilityService.Setup(); err != nil {
+			notifyError("Installer Docp Manager", err.Error())
+		}
+		agentVersions, err := utilityService.FetchAgentVersions()
+		if err != nil {
+			notifyError("Installer Docp Manager", err.Error())
+		}
+		version = agentVersions.LatestVersion
+	}
 	url := prepareUrl(baseUrl, version, fileName)
 
 	pathDir := os.Getenv("ProgramFiles")
