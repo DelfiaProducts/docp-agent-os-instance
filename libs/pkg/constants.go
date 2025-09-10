1521827044f8b60e93a7ea3575b0d46840f39bc5
Tom <wellington.gadelha@delfia.tech>
2025-09-10 09:51:10 -0300

    #1[refac: libs/pkg/constants.go]: update url and name for index json for os instance

--- libs/pkg/constants.go
+++ libs/pkg/constants.go
@@ -3,8 +3,8 @@ package pkg
 const (
 	DOCP_AGENT_PORT = "12012"
 	DOCP_DOMAIN     = "https://msapi.sandbox.docphq.tech"
-	BUCKET_URL      = "https://docp-agent-k8s.s3.us-east-1.amazonaws.com"
+	BUCKET_URL      = "https://docp-agent.s3.us-east-1.amazonaws.com"
 	URL_RELEASE     = "https://github.com/DelfiaProducts/docp-agent-os-instance/releases/download"
 
-	DOCP_FILE_AGENT_VERSIONS_NAME = "index.json"
+	DOCP_FILE_AGENT_VERSIONS_NAME = "index_os_instance.json"
 )
