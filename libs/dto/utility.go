45d8b85e4ba4ac43ec9aed78eb44186c71a12ec3
Tom <wellington.gadelha@delfia.tech>
2025-09-26 08:30:04 -0300

    #26[refac: libs/dto/utility.go]: add dto for datadog api github versions

--- libs/dto/utility.go
+++ libs/dto/utility.go
@@ -5,3 +5,9 @@ type AgentVersions struct {
 	LatestVersion string   `json:"latest"`
 	Versions      []string `json:"versions"`
 }
+
+// DatadogApiGithubVersions struct for datadog api github versions
+type DatadogApiGithubVersions struct {
+	Name    string `json:"name"`
+	TagName string `json:"tag_name"`
+}
