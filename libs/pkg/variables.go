de5e5c1d35f27672acdef5640ab4077733b0f5d3
Tom <wellington.gadelha@delfia.tech>
2025-09-26 08:32:15 -0300

    #26[refac: libs/pkg/variables.go]: add variable for error the failed get version latest datadog

--- libs/pkg/variables.go
+++ libs/pkg/variables.go
@@ -3,15 +3,16 @@ package pkg
 import "errors"
 
 var (
-	ErrNotAuthorized          = errors.New("not authorized")
-	ErrAuthTokenClaimsInvalid = errors.New("invalid token claims")
-	ErrNotFound               = errors.New("not found")
-	ErrSignalAlreadyExists    = errors.New("signal already exists")
-	ErrFailedGetAgentVersions = errors.New("failed get agent versions")
-	ErrValidateUrlExists      = errors.New("url not found")
-	ErrAgentVersionNotFound   = errors.New("agent version not found")
-	ErrDatadogVersionNotFound = errors.New("datadog version not found")
-	ErrContextExpired         = errors.New("context expired")
+	ErrNotAuthorized                 = errors.New("not authorized")
+	ErrAuthTokenClaimsInvalid        = errors.New("invalid token claims")
+	ErrNotFound                      = errors.New("not found")
+	ErrSignalAlreadyExists           = errors.New("signal already exists")
+	ErrFailedGetAgentVersions        = errors.New("failed get agent versions")
+	ErrFailedGetLatestVersionDatadog = errors.New("failed get latest version datadog")
+	ErrValidateUrlExists             = errors.New("url not found")
+	ErrAgentVersionNotFound          = errors.New("agent version not found")
+	ErrDatadogVersionNotFound        = errors.New("datadog version not found")
+	ErrContextExpired                = errors.New("context expired")
 
 	// transactions events
 	TransactionEventOpen   = "open"
