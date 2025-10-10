e799b2cb879b7c2b43e5b43cc7185a7a12e5344f
Tom <wellington.gadelha@delfia.tech>
2025-10-10 17:53:03 -0300

    #32[refac: libs/components/datadog_linux_operations.go]: separate logics on files

--- libs/components/datadog_linux_operations.go
+++ libs/components/datadog_linux_operations.go
@@ -1,14 +1,10 @@
 package components
 
 import (
-	"bufio"
-	"bytes"
 	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
-	"regexp"
-	"strings"
 
 	"github.com/OryaHub/agent-os-instance/libs/interfaces"
 	"github.com/OryaHub/agent-os-instance/libs/pkg"
@@ -36,48 +32,6 @@ func NewDatadogLinuxOperation(logger interfaces.ILogger) *DatadogLinuxOperation 
 	return &DatadogLinuxOperation{
 		logger: logger,
 	}
-}
-
-// prepareEnvs return envs the datadog
-func (d *DatadogLinuxOperation) prepareEnvs(ddSite, ddApiKey string) []string {
-	var envs []string
-	envs = append(envs, fmt.Sprintf("DD_API_KEY=%s", ddApiKey))
-	envs = append(envs, fmt.Sprintf("DD_SITE=%s", ddSite))
-	return envs
-}
-
-// parseDatadogAgentVersion parses the installed version from the output of `apt-cache policy datadog-agent`
-func (d *DatadogLinuxOperation) parseDatadogAgentVersion(output string, prefix string) (string, error) {
-	lines := strings.Split(output, "\n")
-	re := regexp.MustCompile(fmt.Sprintf(`%s\s*([0-9]+:)?([0-9]+\.[0-9]+\.[0-9]+)-[0-9]+`, prefix))
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		if strings.HasPrefix(line, prefix) {
-			matches := re.FindStringSubmatch(line)
-			if len(matches) >= 3 {
-				return matches[2], nil
-			}
-		}
-	}
-	return "", errors.New("installed version not found")
-}
-
-func (d *DatadogLinuxOperation) getVersionFromOutput(output []byte, version string) (string, error) {
-	scanner := bufio.NewScanner(bytes.NewReader(output))
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.Contains(line, "Installed") || strings.Contains(line, "Candidate") {
-			continue
-		}
-		if strings.Contains(line, version) {
-			fields := strings.Fields(line)
-			if len(fields) > 0 {
-				return fields[0], nil
-			}
-		}
-	}
-
-	return "", utils.ErrDatadogVersionNotFound()
 }
 
 func (d *DatadogLinuxOperation) Setup() error {
