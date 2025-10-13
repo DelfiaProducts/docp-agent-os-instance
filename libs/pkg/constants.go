d4587b872d124b61b3dddee2f638cb259ba6107d
Tom <wellington.gadelha@delfia.tech>
2025-10-13 09:26:41 -0300

    #33[refac: libs/pkg/constants.go]: update url for last dns s3

--- libs/pkg/constants.go
+++ libs/pkg/constants.go
@@ -3,7 +3,7 @@ package pkg
 const (
 	ORYA_AGENT_PORT             = "12012"
 	ORYA_DOMAIN                 = "https://msapi.sandbox.oryahq.tech"
-	BUCKET_URL                  = "https://orya-agent.s3.us-east-1.amazonaws.com"
+	BUCKET_URL                  = "https://docp-agent.s3.us-east-1.amazonaws.com"
 	URL_RELEASE                 = "https://github.com/OryaHub/agent-os-instance/releases/download"
 	URL_DATADOG_AGENT_WINDOWS   = "https://s3.amazonaws.com/ddagent-windows-stable"
 	URL_DATADOG_GITHUB_VERSIONS = "https://api.github.com/repos/DataDog/datadog-agent/releases"
