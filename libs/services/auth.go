9e2be417ff8895bdc40030cd4b5bf49930635e9c
Tom <wellington.gadelha@delfia.tech>
2025-10-13 10:05:22 -0300

    #33[refac: libs/services/auth.go]: update api key name

--- libs/services/auth.go
+++ libs/services/auth.go
@@ -63,7 +63,7 @@ func (as *AuthService) AuthCall(payload dto.AuthPayload) ([]byte, int, error) {
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("orya-api-key", apiKey)
+	req.Header.Set("docp-api-key", apiKey)
 
 	res, err := as.client.Do(req)
 	if err != nil {
