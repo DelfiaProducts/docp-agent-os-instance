b57ba1f194b11911b05d471f68e10095a6b35dab
Tom <wellington.gadelha@delfia.tech>
2026-01-05 20:09:38 -0300

    #42[refac: libs/dto/agent_register.go]: update dto for create or update compute

--- libs/dto/agent_register.go
+++ libs/dto/agent_register.go
@@ -1,20 +1,8 @@
 package dto
 
-// AgentRegisterData is struct for data the service agente register
-type AgentRegisterData struct {
-	ClientInfo ClientInfo `json:"client_info"`
-	Metadata   Metadata   `json:"linux_metadata"`
-}
-
-// RegisterAgent is struct for agents
-type RegisterAgent struct {
-	ClientInfo ClientInfo `json:"client_info"`
-	LinuxAgent LinuxAgent `json:"linux_agent"`
-}
-
-// AgentRegisterDataCreate is struct for data the create initial metadata
+// AgentRegisterDataCreateOrUpdate is struct for data the create or update metadata
 // in service agente register
-type AgentRegisterDataCreate struct {
+type AgentRegisterDataCreateOrUpdate struct {
 	NoGroupAssociation bool     `json:"no_group_association,omitempty"`
 	Tags               []string `json:"tags"`
 	VMName             string   `json:"vm_name"`
@@ -34,12 +22,4 @@ type AgentRegisterDataResponseError struct {
 	Code    string      `json:"code,omitempty"`
 	Message string      `json:"message,omitempty"`
 	Data    interface{} `json:"data,omitempty"`
-}
-
-// AgentRegisterDataUpdate is struct for data the update metadata
-// in service agente register
-type AgentRegisterDataUpdate struct {
-	VMName   string   `json:"vm_name"`
-	Tags     []string `json:"tags"`
-	Metadata Metadata `json:"metadata"`
 }
