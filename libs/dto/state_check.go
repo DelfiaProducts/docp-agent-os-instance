e69567d308fd128767cb9b959d8d4cf770852160
Tom <wellington.gadelha@delfia.tech>
2026-01-07 21:35:32 -0300

    #44[refac: libs/dto/state_check.go]: add tracer id for state check response and transaction dto

--- libs/dto/state_check.go
+++ libs/dto/state_check.go
@@ -2,7 +2,8 @@ package dto
 
 // StateCheckResponse is struct for response the state check
 type StateCheckResponse struct {
-	Signal StateCheckSignal `json:"signal"`
+	Signal   StateCheckSignal `json:"signal"`
+	TracerId string           `json:"tracer_id"`
 }
 
 // StateCheckSignal is struct for signal
@@ -79,6 +80,7 @@ const ContextTransactionStatus ctxKey = "transactionStatus"
 // TransactionStatus is struct for transaction status
 type TransactionStatus struct {
 	ID        string `json:"id"`
+	TracerId  string `json:"tracer_id,omitempty"`
 	UlidEvent string `json:"ulid_event"`
 	TypeEvent string `json:"type"`
 	Status    string `json:"status"`
