7275f533fa658ee55bb6f0dee122d1b81e29d7f2
gadelhatom <wellington.gadelha@delfia.tech>
2026-01-09 17:02:16 -0300

    Fix/resolve transaction status (#46)
    * [refac: Makefile.dev]: add version env for test local
    
    * [refac: libs/dto/state_check.go]: remove omitempty for force send trace id when empty

--- libs/dto/state_check.go
+++ libs/dto/state_check.go
@@ -80,7 +80,7 @@ const ContextTransactionStatus ctxKey = "transactionStatus"
 // TransactionStatus is struct for transaction status
 type TransactionStatus struct {
 	ID        string `json:"id"`
-	TraceId   string `json:"trace_id,omitempty"`
+	TraceId   string `json:"trace_id"`
 	UlidEvent string `json:"ulid_event"`
 	TypeEvent string `json:"type"`
 	Status    string `json:"status"`
