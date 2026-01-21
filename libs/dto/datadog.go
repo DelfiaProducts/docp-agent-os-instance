52c22b6394160f78ca0e3df0968033627790b0a8
Tom <wellington.gadelha@delfia.tech>
2026-01-21 15:45:38 -0300

    #47[refac: libs/dto/datadog.go]: add datadog infos

--- libs/dto/datadog.go
+++ libs/dto/datadog.go
@@ -33,3 +33,20 @@ type DatadogYamlDTO struct {
 	ApiKey string `yaml:"api_key"`
 	Site   string `yaml:"site"`
 }
+
+type DatadogInfos struct {
+	HostId                        string `yaml:"hostId" json:"host_id"`
+	Hostname                      string `yaml:"hostname" json:"hostname"`
+	KernelArch                    string `yaml:"kernelArch" json:"kernel_arch"`
+	KernelVersion                 string `yaml:"kernelVersion" json:"kernel_version"`
+	Os                            string `yaml:"os" json:"os"`
+	Platform                      string `yaml:"platform" json:"platform"`
+	PlatformFamily                string `yaml:"platformFamily" json:"platform_family"`
+	PlatformVersion               string `yaml:"platformVersion" json:"platform_version"`
+	AgentVersion                  string `yaml:"agent_version" json:"agent_version"`
+	Flavor                        string `yaml:"flavor" json:"flavor"`
+	InfrastructureMode            string `yaml:"infrastructure_mode" json:"infrastructure_mode"`
+	InstallMethodInstallerVersion string `yaml:"install_method_installer_version" json:"install_method_installer_version"`
+	InstallMethodTool             string `yaml:"install_method_tool" json:"install_method_tool"`
+	InstallMethodToolVersion      string `yaml:"install_method_tool_version" json:"install_method_tool_version"`
+}
