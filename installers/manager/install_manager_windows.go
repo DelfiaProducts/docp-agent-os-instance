e5449630f444da55420e54fc2257f11a3073bb3e
Tom <wellington.gadelha@delfia.tech>
2026-05-29 08:46:41 -0300

    #64 refac: update format to 'api_key' from 'apiKey'

--- installers/manager/install_manager_windows.go
+++ installers/manager/install_manager_windows.go
@@ -27,7 +27,7 @@ type ConfigAgent struct {
 
 // Agent is struct for config file agent
 type Agent struct {
-	ApiKey string            `yaml:"apiKey"`
+	ApiKey string            `yaml:"api_key"`
 	Tags   map[string]string `yaml:"tags"`
 }
 
