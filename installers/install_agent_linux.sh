6e7305ea3a3547043c3ecd2837e5f582186f6ed9
Tom <wellington.gadelha@delfia.tech>
2026-05-29 08:53:20 -0300

    #65 refac: add progress messages to installation steps in install_agent_linux.sh

--- installers/install_agent_linux.sh
+++ installers/install_agent_linux.sh
@@ -47,6 +47,7 @@ fi
 
 #verify is architecture and get binary
 function verify_architecture(){
+printf "Checking system architecture...\n"
 if [[ "$ARCHITECTURE" == "aarch64" ]]; then
   get_binary_arch64
 fi
@@ -63,6 +64,7 @@ function setup(){
 
 # Corrige a função resolve_version para extrair corretamente o campo "latest" do JSON
 function resolve_version() {
+  printf "Resolving version...\n"
   local version="$1"
   if [[ "$version" == "latest" ]]; then
     version=$(curl -s "$FILE_INDEX_URL" | sed -n 's/.*"latest"[[:space:]]*:[[:space:]]*"\([^"]*\)".*/\1/p')
@@ -76,31 +78,37 @@ function resolve_version() {
 
 #add permission workdir
 function add_perm_work_dir(){
+  printf "Adding permissions to work directory...\n"
   $sudo_cmd chown -R $USER_GROUP_NAME:$USER_GROUP_NAME /opt/orya-agent/
 }
 
 #get binary arm64
 function get_binary_arch64(){
+  printf "Downloading binary for arm64 architecture...\n"
   sudo curl -s -L -o $ORYA_FILES_PATH/bin/releases/$VERSION/agent "$BINARY_URL/$VERSION/agent-linux-arm64"
   sudo chmod +x $ORYA_FILES_PATH/bin/releases/$VERSION/agent
 }
 #get binary amd64
 function get_binary_amd64(){
+  printf "Downloading binary for amd64 architecture...\n"
   sudo curl -s -L -o $ORYA_FILES_PATH/bin/releases/$VERSION/agent "$BINARY_URL/$VERSION/agent-linux-amd64"
   sudo chmod +x $ORYA_FILES_PATH/bin/releases/$VERSION/agent
 }
 # create symbolic link
 function create_link_simbolic(){
+  printf "Creating symbolic link for agent...\n"
   sudo ln -sfn $ORYA_FILES_PATH/bin/releases/$VERSION/agent $ORYA_FILES_PATH/bin/current/agent
 }
 
 #set content service
 function set_content_service() {
+  printf "Creating systemd service file...\n"
   printf "[Unit]\nDescription=Orya Agent\nAfter=network.target\n\n[Service]\nType=simple\nPIDFile=/opt/orya-agent/run/agent.pid\nUser=orya-agent\nRestart=on-failure\nEnvironmentFile=-/opt/orya-agent/environments\nRuntimeDirectory=orya\nExecStart=/opt/orya-agent/bin/current/agent run -p /opt/orya-agent/run/agent.pid\nStartLimitInterval=10\nStartLimitBurst=5\nStandardOutput=journal\nStandardError=journal\n\n[Install]\nWantedBy=multi-user.target\n" | sudo tee /etc/systemd/system/orya-agent.service > /dev/null
 }
 
 #prepare systemd
 function prepare_systemd() {
+  printf "Setting up systemd service...\n"
   $sudo_cmd systemctl daemon-reload
   $sudo_cmd systemctl start orya-agent.service
   $sudo_cmd systemctl enable orya-agent.service
@@ -113,3 +121,4 @@ create_link_simbolic
 add_perm_work_dir
 set_content_service
 prepare_systemd
+printf "\033[32mAgent installed successfully\033[0m\n"
